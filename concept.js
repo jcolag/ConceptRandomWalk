16dfd6c7da7871c868617d8242136b6624522645
John Colagioia <john@colagioia.net>
2018-10-21 07:30:18 -0400

    Prefer local files to avoid hammering API

--- concept.js
+++ concept.js
@@ -2,14 +2,29 @@ const rest = require('rest');
 const fs = require('fs');
 
 var word = 'example';
+var graph;
 if (process.argv.length > 2) {
   word = process.argv[process.argv.length - 1];
 }
+
+if (fs.existsSync(`${word}.json`)) {
+  console.log(`Already have "${word}"...`);
+  graph = JSON.parse(fs.readFileSync(`${word}.json`))
+  processWord(graph);
+  return;
+}
+
 rest(`http://api.conceptnet.io/c/en/${word}`)
   .then(r => {
     const relations = {};
-    const result = JSON.parse(r.entity);
-    fs.writeFileSync(`${word}.json`, JSON.stringify(result, null, 4));
-    console.log(`Written to ${word}.json`);
+    graph = JSON.parse(r.entity);
+    fs.writeFileSync(`${word}.json`, JSON.stringify(graph, null, 4));
+    console.log(`New word written to ${word}.json...`);
+    processWord(graph);
   });
+
+function processWord(wordObj) {
+  console.log(wordObj.edges.length);
+}
+
 
